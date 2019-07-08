6e5c5b8167fe53585872f6cd8237dea8cb765199
Arno Bats <arnobats@gmail.com>
2019-07-08 16:51:20 +0200

    initial commit, vue setup, tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="md:bg-gray-400 m-5">
+  <div class="ml-5">
+    Recipe list
   </div>
+  <div class="md:flex md:mt-6 max-w-xl md:bg-white rounded-lg md:m-5 md:mt-5">
+    <div class="md:flex-shrink-0">
+      <img class="rounded-lg md:w-56" src="@/assets/image/recipe.jpg" alt="something delicious">
+    </div>
+    <div class="mt-4 md:mt-0 md:ml-6">
+      <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Super recipe</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">This is super delicious and will take you only 20 minutes to make</a>
+      <p class="mt-2 text-gray-600">Bla die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh </p>
+    </div>
+  </div>
+
+  <div class="md:flex md:mt-6 max-w-xl md:bg-white rounded-lg md:m-5 md:mt-5">
+    <div class="md:flex-shrink-0">
+      <img class="rounded-lg md:w-56" src="@/assets/image/recipe.jpg" alt="something delicious">
+    </div>
+    <div class="mt-4 md:mt-0 md:ml-6">
+      <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Super recipe</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">This is super delicious and will take you only 20 minutes to make</a>
+      <p class="mt-2 text-gray-600">Bla die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh </p>
+    </div>
+  </div>
+
+  <div class="md:flex md:mt-6 max-w-xl md:bg-white rounded-lg md:m-5 md:mt-5">
+    <div class="md:flex-shrink-0">
+      <img class="rounded-lg md:w-56" src="@/assets/image/recipe.jpg" alt="something delicious">
+    </div>
+    <div class="mt-4 md:mt-0 md:ml-6">
+      <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Super recipe</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">This is super delicious and will take you only 20 minutes to make</a>
+      <p class="mt-2 text-gray-600">Bla die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh die bla die blaaahhh </p>
+    </div>
+  </div>
+
+</div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+<script>
+export default {
+}
 </script>
